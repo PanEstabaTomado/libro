859add0f347d7798e3e3418a16786d2b3b91696f
pan <panamasado41@gmail.com>
2026-04-28 16:26:26 -0400

    Inicial

---
+++ src/main/java/dfs1103/maq/bibiliotecaam/BibiliotecaamApplication.java
@@ -0,0 +1,13 @@
+package dfs1103.maq.bibiliotecaam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibiliotecaamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibiliotecaamApplication.class, args);
+	}
+
+}
