e0b4189b41b59394b0c91db0af48dd82cb697c63
Alfonso Pizarro <ajpizarror@gmail.com>
2026-05-01 11:23:32 -0400

    librorequestdt y libroresponse dto

--- src/main/java/dfs1103/maq/bibiliotecaam/dto/LibroRequestDTO.java
+++ src/main/java/dfs1103/maq/bibiliotecaam/dto/LibroRequestDTO.java
@@ -1,4 +1,25 @@
 package dfs1103.maq.bibiliotecaam.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibroRequestDTO {
+
+    @NotBlank(message = "El ID del libro no puede estar vacio.")
+    private Long id_libro;
+    @NotBlank(message = "El isbn no puede estar vacio.")
+    private String isbn;
+    @NotBlank(message = "El titulo no puede estar vacio.")
+    private String titulo;
+    @NotBlank(message = "El autor del libro no puede estar vacio")
+    private String autor;
+    @NotNull(message = "El campo prestado debe ser definido.")
+    private boolean prestado;
+
 }
--- src/main/java/dfs1103/maq/bibiliotecaam/dto/LibroResponseDTO.java
+++ src/main/java/dfs1103/maq/bibiliotecaam/dto/LibroResponseDTO.java
@@ -1,4 +1,19 @@
 package dfs1103.maq.bibiliotecaam.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibroResponseDTO {
+
+    private Long id_libro;
+    private String isbn;
+    private String titulo;
+    private String autor;
+    private boolean prestado;
+    private Long id_dona;
+
 }
