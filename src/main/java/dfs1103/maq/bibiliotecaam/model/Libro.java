0872681d1ad2b3ace37a15943bd270226d324ddd
Alfonso Pizarro <ajpizarror@gmail.com>
2026-05-01 11:08:42 -0400

    clase libro modificada

--- src/main/java/dfs1103/maq/bibiliotecaam/model/Libro.java
+++ src/main/java/dfs1103/maq/bibiliotecaam/model/Libro.java
@@ -1,4 +1,38 @@
 package dfs1103.maq.bibiliotecaam.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "libro")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_libro;
+
+    @Column(nullable = false, length = 20, unique = true)
+    private String isbn;
+
+    @Column(nullable = false, length = 20)
+    private String titulo;
+
+    @Column(nullable = false, length = 20)
+    private String autor;
+
+    @Column(nullable = false)
+    private boolean prestado;
+
+    @Column(nullable = true, precision = 10, scale = 2)
+    private BigDecimal precio;
+
+    @Column(nullable = true, precision = 10)
+    private Integer id_dona;
+
 }
