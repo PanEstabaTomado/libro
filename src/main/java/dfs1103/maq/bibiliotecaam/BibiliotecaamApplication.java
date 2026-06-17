6181c251d29d47a754d45d26974dcc1758003a29
VI-Alumno <VI-Alumno@duoc.acad>
2026-06-17 10:01:51 -0400

    Conectando a Eureka, funciona.

--- src/main/java/dfs1103/maq/bibiliotecaam/BibiliotecaamApplication.java
+++ src/main/java/dfs1103/maq/bibiliotecaam/BibiliotecaamApplication.java
@@ -2,8 +2,10 @@ package dfs1103.maq.bibiliotecaam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BibiliotecaamApplication {
 
 	public static void main(String[] args) {
